9a74661c8645e449cbb89a0078c97f0b5477d13f
Mogens Heller Grabe <mookid8000@gmail.com>
2019-03-07 11:00:37 +0100

    even more thorough disposal of async tasks

---
+++ Rebus.AzureServiceBus.Tests/Bugs/VerifyAssumptionAboutDisposingAsyncTaskMultipleTimes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Rebus.Logging;
+using Rebus.Tests.Contracts;
+using Rebus.Threading.TaskParallelLibrary;
+
+namespace Rebus.AzureServiceBus.Tests.Bugs
+{
+    [TestFixture]
+    public class VerifyAssumptionAboutDisposingAsyncTaskMultipleTimes : FixtureBase
+    {
+        [Test]
+        public async Task NoProblem()
+        {
+            var consoleLoggerFactory = new ConsoleLoggerFactory(colored: false);
+            var factory = new TplAsyncTaskFactory(consoleLoggerFactory);
+            var logger = consoleLoggerFactory.GetLogger<VerifyAssumptionAboutDisposingAsyncTaskMultipleTimes>();
+            var task = factory.Create("test-task", intervalSeconds: 1, action: async () => logger.Info("Called back"));
+
+            task.Start();
+
+            await Task.Delay(TimeSpan.FromSeconds(4));
+
+            task.Dispose();
+            task.Dispose();
+            task.Dispose();
+            task.Dispose();
+        }
+        
+    }
+}
