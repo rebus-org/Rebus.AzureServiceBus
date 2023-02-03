9bbe87abcaf23aa73cccc3077a66ba45635e6c50
Mogens Heller Grabe <mookid8000@gmail.com>
2023-02-03 08:39:11 +0100

    add / in queue/topic names example

---
+++ Rebus.AzureServiceBus.Tests/Checks/CanIndeedHaveTopicNameWithForwardSlash.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus.Administration;
+using NUnit.Framework;
+using Rebus.AzureServiceBus.Tests.Bugs;
+using Rebus.Internals;
+using Rebus.Tests.Contracts;
+
+namespace Rebus.AzureServiceBus.Tests.Checks;
+
+[TestFixture]
+[Description("Just an example that shows that you can indeed use / in topic names")]
+public class CanIndeedHaveTopicNameWithForwardSlash : FixtureBase
+{
+    const string QueueName = "MyServiceBus/MyConsumer";
+    const string TopicName = "MyServiceBus/MyServiceBus.Messages.MySomethingSomethingMessage";
+
+    protected override void SetUp()
+    {
+        base.SetUp();
+
+        Using(new TopicDeleter(TopicName));
+        Using(new QueueDeleter(QueueName));
+    }
+
+    [Test]
+    public async Task LookItWorks()
+    {
+        var admin = new ServiceBusAdministrationClient(AsbTestConfig.ConnectionString);
+        
+        await admin.CreateTopicAsync(TopicName);
+        await admin.CreateQueueIfNotExistsAsync(QueueName);
+    }
+}
