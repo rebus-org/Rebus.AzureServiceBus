f38631beb77d96d23fcd67c18d84c88b3e17dce2
Mogens Heller Grabe <mookid8000@gmail.com>
2024-10-13 10:21:33 +0200

    add example

---
+++ Rebus.AzureServiceBus.Tests/Examples/TheMostInterestingHandlerInTheWorldWorks.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Rebus.Activation;
+using Rebus.AzureServiceBus.Tests.Bugs;
+using Rebus.Config;
+using Rebus.Handlers;
+using Rebus.Tests.Contracts;
+using Rebus.Tests.Contracts.Utilities;
+using Rebus.Transport.InMem;
+
+namespace Rebus.AzureServiceBus.Tests.Examples;
+
+[TestFixture]
+public class TheMostInterestingHandlerInTheWorldWorks : FixtureBase
+{
+    [Test]
+    public async Task OfCourseHandlerCanBeSimpleLikeThis()
+    {
+        var queueName = Guid.NewGuid().ToString("n");
+
+        Using(new QueueDeleter(queueName));
+
+        var logs = new ListLoggerFactory();
+
+        using var activator = new BuiltinHandlerActivator();
+
+        activator.Register(() => new TheMostInterestingHandlerInTheWorld());
+
+        var bus = Configure.With(activator)
+            .Logging(l => l.Use(logs))
+            .Transport(t => t.UseAzureServiceBus(AsbTestConfig.ConnectionString, queueName))
+            .Start();
+
+        await bus.SendLocal(new MyMessage());
+
+        // wait a short while
+        await Task.Delay(TimeSpan.FromSeconds(1));
+
+        Assert.That(logs.Select(line => line.Text), Does.Not.Contain("InvalidOperationException"));
+    }
+
+    record MyMessage;
+
+    class TheMostInterestingHandlerInTheWorld : IHandleMessages<MyMessage>
+    {
+        public Task Handle(MyMessage message)
+        {
+            Console.WriteLine("Receive my message");
+            return Task.CompletedTask;
+        }
+    }
+}
