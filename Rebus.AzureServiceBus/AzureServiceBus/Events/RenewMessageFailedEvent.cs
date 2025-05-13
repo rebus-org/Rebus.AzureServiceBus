5346cc60dc608b072da4e8a1d171494a200b8166
Henrik Drachmann <hdr@raptor.dk>
2025-05-13 11:00:08 +0200

    removed ununsed event

--- Rebus.AzureServiceBus/AzureServiceBus/Events/RenewMessageFailedEvent.cs
+++ Rebus.AzureServiceBus/AzureServiceBus/Events/RenewMessageFailedEvent.cs
@@ -5,10 +5,5 @@ using System.Text;
 namespace Rebus.AzureServiceBus.Events
 {
     public delegate void RenewMessageFailedHandler(string messageId, Exception exception);
-    public class RebusEvents
-    {
-        public event RenewMessageFailedHandler RenewMessageFailed;
-    }
-
-
+   
 }
